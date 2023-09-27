967a91fb033ae12f8f8dc2818ba7bf60d932770e
Steven Valladares <stevenvalladaresgiron@gmail.com>
2023-09-26 23:57:50 -0300

    chore(setup): :wrench: bootstrap is added to project configuration
    bootstrap styles, scripts and icons are imported into the main main.jsx and app.jsx files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 // import { useState } from 'react'
 
 function App() {
-  return <>{/* Add HTML code here */}</>;
+  return (
+    <>
+      <div className="container py-4 px-3 mx-auto">
+        <h1>
+          Hello, <i className="bi bi-bootstrap"></i> and Vite!
+        </h1>
+        <button className="btn btn-primary">Primary button</button>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+
+// Import CSS and JS styles from Bootstrap
+import './assets/scss/styles.scss';
+import * as bootstrap from 'bootstrap';
+
+// Import all Bootstrap icons
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
